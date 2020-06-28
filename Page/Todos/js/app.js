02cf76e78191e928739b6780cacf0037fe839248
jxlove2020-PC\jxlove2020 <jxlove2020@gmail.com>
2020-06-28 20:22:17 +0900

    검색시 대소문자 구분 없이 검색 가능하도록 변경

--- Page/Todos/js/app.js
+++ Page/Todos/js/app.js
@@ -29,7 +29,7 @@ list.addEventListener('click', e => {
 // filter
 const filterTodos = (term) => {
     Array.from(list.children)
-        .filter((todo) => !todo.textContent.includes(term))
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term))
         .forEach((todo) => todo.classList.add('filtered'));
     Array.from(list.children)
         .filter((todo) => todo.textContent.includes(term))
