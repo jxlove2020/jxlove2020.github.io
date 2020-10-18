17a763e53ab9558b9d63d0a4b18a52ac9ee16be3
JXLOVE2020\jxlov <jxlove2020@gmail.com>
2020-10-19 04:27:53 +0900

    loader study

--- NodeJs/webpack/sample/webpack.config.js
+++ NodeJs/webpack/sample/webpack.config.js
@@ -9,5 +9,15 @@ module.exports = {
     output: {
         path: path.resolve('./dist'),
         filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    path.resolve('./my-webpack-loader.js')
+                ]
+            }
+        ]
     }
 }
