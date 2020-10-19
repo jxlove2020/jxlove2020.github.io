3bd2f8178c4aeffbe32edbb61b7df1790b160d25
jxlove2020 <jxlove2020@gmail.com>
2020-10-19 16:28:22 +0900

    첫페이지 수정

--- NodeJs/nodejs-first-app/app.js
+++ NodeJs/nodejs-first-app/app.js
@@ -86,5 +86,5 @@ app.post('/topic', function(req, res){
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/topic`);
 });
